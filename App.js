16d491d007af513f1f72e2b12df9180ea63daad4
elinarou <elina_rouvinen@outlook.com>
2023-01-30 18:29:44 +0200

    Laskin historialla

--- App.js
+++ App.js
@@ -1,48 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
   const [numbers, setNumbers] = useState({num1: 0, num2: 0});
   const [result, setResult] = useState(0);
   const [ready, setReady] = useState(false);
+  const [data, setData] = useState([]);
 
   const buttonPressed = (operator) => {
     if (operator == "+") {
       setResult(parseInt(numbers.num1) + parseInt(numbers.num2));
+      setData([...data, 
+        {key: numbers.num1 + ' ' + operator + ' ' + numbers.num2 + ' = ' + 
+          (parseInt(numbers.num1) + parseInt(numbers.num2))
+        }]);
     }
     else if (operator == "-") {
       setResult(numbers.num1 - numbers.num2);
-    }
-    setReady(true); 
+      setData([...data, 
+        {key: numbers.num1 + ' ' + operator + ' ' + numbers.num2 + ' = ' + 
+          (numbers.num1 - numbers.num2)
+        }]);
+    };
+    setReady(true);
+    setNumbers({num1: 0, num2: 0});
   }; 
 
   return (
     <View style={styles.container}>
-      <View>
-        {ready == true && <Text>Result: {result}</Text>}
+
+      <View style={styles.subcontainer1}>
+
+        {ready && <Text>Result: {result}</Text>}
+
+        <TextInput
+          style={styles.input} 
+          onChangeText={text => setNumbers({...numbers, num1: text})} 
+          value={numbers.num1}
+          keyboardType="numeric" 
+        />
+        <TextInput 
+          style={styles.input} 
+          onChangeText={text => setNumbers({...numbers, num2: text})} 
+          value={numbers.num2}
+          keyboardType="numeric" 
+        />
+
+        <View style={styles.operators}>
+          <View style={styles.button}>
+            <Button 
+              onPress={() => buttonPressed("+")} 
+              title="+"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button 
+              onPress={() => buttonPressed("-")} 
+              title="-"
+            />
+          </View>  
+        </View>
+
       </View>
-      <TextInput 
-        style={styles.input} 
-        onChangeText={text => setNumbers({...numbers, num1: text})} 
-        value={numbers.num1}
-        keyboardType="numeric" 
-      />
-      <TextInput 
-        style={styles.input} 
-        onChangeText={text => setNumbers({...numbers, num2: text})} 
-        value={numbers.num2}
-        keyboardType="numeric" 
-      />
-      <View style={styles.button}>
-        <Button
-          onPress={() => buttonPressed("+")} 
-          title="+"
+
+      <View style={styles.subcontainer2}>
+        
+        {ready && <Text>History:</Text>}
+       
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
         />
-        <Button
-          onPress={() => buttonPressed("-")} 
-          title="-"
-        />
+
       </View>
       <StatusBar style="auto" />
     </View>
@@ -57,15 +88,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  subcontainer1: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 200,
+    marginBottom: 10
+  },
+
+  subcontainer2: {
+    flex: 1
+  },
+
   input : {
     width:200  , 
     borderColor: 'gray', 
-    borderWidth: 1
+    borderWidth: 1,
+    padding: 5,
+    margin: 10
+  },
+
+  operators : {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
 
   button : {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10
-  }
+    width: '20%',
+    margin: 10
+  },
 });
